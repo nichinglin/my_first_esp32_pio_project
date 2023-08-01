31c064e2147701549675dc344a444e0766552b45
MiNiMac <mini@MiNiMacs-Mac-mini.local>
2023-08-01 16:51:47 +0800

    update test

---
+++ src/canon_in_d.cpp
@@ -0,0 +1,57 @@
+// /*
+//   Rui Santos
+//   Complete project details at https://RandomNerdTutorials.com/esp32-stepper-motor-28byj-48-uln2003/
+  
+//   Permission is hereby granted, free of charge, to any person obtaining a copy
+//   of this software and associated documentation files.
+  
+//   The above copyright notice and this permission notice shall be included in all
+//   copies or substantial portions of the Software.
+  
+//   Based on Stepper Motor Control - one revolution by Tom Igoe
+// */
+// #include <Arduino.h>
+// #include <Buzzer.h>
+
+// Buzzer buzzer(3);
+
+// void setup() {}
+
+// void loop() {
+//   buzzer.begin(10);
+
+//   buzzer.sound(NOTE_A4, 500); 
+//   buzzer.sound(NOTE_FS4, 250); 
+//   buzzer.sound(NOTE_G4, 250); 
+//   buzzer.sound(NOTE_A4, 500); 
+//   buzzer.sound(NOTE_FS4, 250); 
+//   buzzer.sound(NOTE_G4, 250);
+
+//   buzzer.sound(NOTE_A4, 250); 
+//   buzzer.sound(NOTE_A3, 250); 
+//   buzzer.sound(NOTE_B3, 250); 
+//   buzzer.sound(NOTE_CS4, 250); 
+//   buzzer.sound(NOTE_D4, 250); 
+//   buzzer.sound(NOTE_E4, 250); 
+//   buzzer.sound(NOTE_FS4, 250); 
+//   buzzer.sound(NOTE_G4, 250); 
+  
+//   buzzer.sound(NOTE_FS4, 500); 
+//   buzzer.sound(NOTE_D4, 250); 
+//   buzzer.sound(NOTE_E4, 250); 
+//   buzzer.sound(NOTE_FS4, 500); 
+//   buzzer.sound(NOTE_FS3, 250); 
+//   buzzer.sound(NOTE_G3, 250);
+
+//   buzzer.sound(NOTE_A3, 250); 
+//   buzzer.sound(NOTE_B3, 250); 
+//   buzzer.sound(NOTE_A3, 250); 
+//   buzzer.sound(NOTE_G3, 250); 
+//   buzzer.sound(NOTE_A3, 250); 
+//   buzzer.sound(NOTE_FS3, 250); 
+//   buzzer.sound(NOTE_G3, 250); 
+//   buzzer.sound(NOTE_A3, 250); 
+//   // buzzer.sound(NOTE_, 250); 
+
+//   buzzer.end(2000);
+// }
